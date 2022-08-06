195fe57ecfd8bce273956dd8c6e024d4c5974337
abr-eurus <ammar.bin.rizwan@eurustechnologies.com>
2022-08-06 17:49:14 +0500

    db keys added

--- startup/db.js
+++ startup/db.js
@@ -4,16 +4,15 @@ const constants = require('../app/helpers/constants.helpers')
 module.exports = async () => {
     const connectionString = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
 
-    // const connectionString = 'mongodb://localhost/students'
     try {
         const options = {
             useNewUrlParser: true,
             useCreateIndex: true,
-            useUnifiedTopology: true
-            //  keepAlive: 1,
-            //  connectTimeoutMS: 30000,
-            //  reconnectTries: 30,
-            //  reconnectInterval: 5000,
+            useUnifiedTopology: true,
+             keepAlive: 1,
+             connectTimeoutMS: 30000,
+             reconnectTries: 30,
+             reconnectInterval: 5000
         }
         await mongoose.connect(connectionString, options)
         console.log(constants.DB_CONNECTED)
