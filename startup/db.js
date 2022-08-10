f4eb51413ccd4bdf1e13f3806e15c40c4ebb1199
abr-eurus <ammar.bin.rizwan@eurustechnologies.com>
2022-08-10 17:21:49 +0500

    mongodb atlas added

--- startup/db.js
+++ startup/db.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose');
 const constants = require('../app/helpers/constants.helpers')
 
 module.exports = async () => {
-    const connectionString = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
+    // const connectionString = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
+    const connectionString = 'mongodb+srv://laith_ajail:crqhFZTEgGijck2L@cluster0.r2klhq2.mongodb.net/tca?retryWrites=true&w=majority';
 
     try {
         const options = {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true,
-             keepAlive: 1,
-             connectTimeoutMS: 30000,
-             reconnectTries: 30,
-             reconnectInterval: 5000
+            // keepAlive: 1,
+            // connectTimeoutMS: 30000,
+            // reconnectTries: 30,
+            // reconnectInterval: 5000
         }
         await mongoose.connect(connectionString, options)
         console.log(constants.DB_CONNECTED)
